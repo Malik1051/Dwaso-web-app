93bbf8c82b789b20697b1dd8afbf56f7906d4aae
malik1051 <benmaleek271@gmail.com>
2025-04-14 18:36:51 +0000

    landing page successfully created and routed

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ def get_products():
 # Landing page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index_landing.html')
 
 # Route for Admin pages
 @app.route('/ad_login', methods=['GET', 'POST'])
